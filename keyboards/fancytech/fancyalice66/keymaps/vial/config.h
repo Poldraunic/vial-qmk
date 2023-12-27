e6e4cb12527b5d065db42d2dbc4a6bc89ca489e1
Egor Krugletsov <krugletsov.egor@gmail.com>
2023-12-27 23:07:54 +0300

    Add limited support for FancyAlice66
    Firmware in its default configuration doesn't fit into chip's available memory. I had to disable all of features I don't need until it fit. The only thing I didn't turn off is Tap Dance.

---
+++ keyboards/fancytech/fancyalice66/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x18, 0x1D, 0xBA, 0x12, 0x6B, 0x70, 0x7C, 0xEC}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
